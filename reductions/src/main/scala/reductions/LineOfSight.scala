05ae2161f881e9ae055626df8a971df7280dd8d9
syhan <syhannnn@gmail.com>
2017-05-11 00:26:06 +0800

    Assignment 2

---
+++ reductions/src/main/scala/reductions/LineOfSight.scala
@@ -0,0 +1,133 @@
+package reductions
+
+import org.scalameter._
+import common._
+
+object LineOfSightRunner {
+  
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 100,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]) {
+    val length = 10000000
+    val input = (0 until length).map(_ % 100 * 1.0f).toArray
+    val output = new Array[Float](length + 1)
+    val seqtime = standardConfig measure {
+      LineOfSight.lineOfSight(input, output)
+    }
+    println(s"sequential time: $seqtime ms")
+
+    val partime = standardConfig measure {
+      LineOfSight.parLineOfSight(input, output, 10000)
+    }
+    println(s"parallel time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+}
+
+object LineOfSight {
+
+  def max(a: Float, b: Float): Float = if (a > b) a else b
+
+  def lineOfSight(input: Array[Float], output: Array[Float]): Unit = {
+    var i = 1
+    var m = 0f
+    while (i < input.length) {
+      m = max(m, input(i) / i)
+      output(i) = m
+
+      i = i + 1
+    }
+    output(0) = 0
+  }
+
+  sealed abstract class Tree {
+    def maxPrevious: Float
+  }
+
+  case class Node(left: Tree, right: Tree) extends Tree {
+    val maxPrevious = max(left.maxPrevious, right.maxPrevious)
+  }
+
+  case class Leaf(from: Int, until: Int, maxPrevious: Float) extends Tree
+
+  /** Traverses the specified part of the array and returns the maximum angle.
+   */
+  def upsweepSequential(input: Array[Float], from: Int, until: Int): Float = {
+    val output: Array[Float] = new Array[Float](input.length)
+    lineOfSight(input, output)
+
+    val o = output.slice(from, until)
+
+    var i: Int = 0
+    var m: Float = 0f
+
+    while (i < o.length) {
+      m = max(m, o(i))
+
+      i = i + 1
+    }
+
+    m
+  }
+
+  /** Traverses the part of the array starting at `from` and until `end`, and
+   *  returns the reduction tree for that part of the array.
+   *
+   *  The reduction tree is a `Leaf` if the length of the specified part of the
+   *  array is smaller or equal to `threshold`, and a `Node` otherwise.
+   *  If the specified part of the array is longer than `threshold`, then the
+   *  work is divided and done recursively in parallel.
+   */
+  def upsweep(input: Array[Float], from: Int, end: Int,
+    threshold: Int): Tree = {
+    if (end - from <= threshold) Leaf(from, end, upsweepSequential(input, from, end))
+    else {
+      val m = (from + end) / 2
+      val (l, r) = parallel(upsweep(input, from, m, threshold), upsweep(input, m, end, threshold))
+
+      Node(l, r)
+    }
+
+  }
+
+  /** Traverses the part of the `input` array starting at `from` and until
+   *  `until`, and computes the maximum angle for each entry of the output array,
+   *  given the `startingAngle`.
+   */
+  def downsweepSequential(input: Array[Float], output: Array[Float],
+    startingAngle: Float, from: Int, until: Int): Unit = {
+    var i = from
+    var m: Float = startingAngle
+
+    while (i < until && i < input.length) {
+      m = max(m, input(i) / i)
+      output(i) = m
+
+      i = i + 1
+    }
+  }
+
+  /** Pushes the maximum angle in the prefix of the array to each leaf of the
+   *  reduction `tree` in parallel, and then calls `downsweepTraverse` to write
+   *  the `output` angles.
+   */
+  def downsweep(input: Array[Float], output: Array[Float], startingAngle: Float,
+    tree: Tree): Unit = {
+    tree match {
+      case Leaf(from, end, maxPrevious) => downsweepSequential(input, output, maxPrevious, from, end)
+      case Node(left, right) => parallel(downsweep(input, output, startingAngle, left), downsweep(input, output, startingAngle, right))
+    }
+  }
+
+  /** Compute the line-of-sight in parallel. */
+  def parLineOfSight(input: Array[Float], output: Array[Float],
+    threshold: Int): Unit = {
+    val tree = upsweep(input, 0, input.length, threshold)
+    downsweep(input, output, 0, tree)
+  }
+}
---
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,116 @@
+package reductions
+
+import scala.annotation._
+import org.scalameter._
+import common._
+
+import scala.runtime.Nothing$
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime ms")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime ms")
+    println(s"speedup: ${seqtime / fjtime}")
+  }
+}
+
+object ParallelParenthesesBalancing {
+
+  /** Returns `true` if the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+    /*def iter(l: List[Char], acc: Int): Int = {
+      l match {
+        case Nil => acc
+        case c::xs => {
+          c match {
+            case '(' => iter(xs, acc + 1)
+            case ')' => iter(xs, Math.abs(acc - 1))
+            case _   => iter(xs, acc)
+          }
+        }
+      }
+    }
+
+    iter(chars.toList, 0) == 0*/
+
+    var i, b = 0
+    while (i < chars.length && b >= 0) {
+      val c = chars(i)
+      if (c == '(') {
+        b = b + 1
+      } else if (c == ')') {
+        b = b - 1
+      }
+
+      i = i + 1
+    }
+
+    b == 0
+  }
+
+  /** Returns `true` if the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      var i = idx
+      var left, right = 0
+      while (i < until) {
+        val c = chars(i)
+        if (c == '(') {
+          left = left + 1
+        } else if (c == ')') {
+          right = right + 1
+        }
+
+        i = i + 1
+      }
+
+      (left, right)
+    }
+
+    def reduce(from: Int, until: Int) : Int = {
+      if (until - from <= threshold) {
+        val (l, r) = traverse(from, until, 0, 0)
+        l - r
+      } else {
+        val m = (until + from) / 2
+        val (a, b) = parallel(reduce(from, m), reduce(m, until))
+
+        a + b
+      }
+
+    }
+
+    reduce(0, chars.length) == 0
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+}
