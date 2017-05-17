cf67d5c58b7197e167f3ed0d7a1c2a64c5ba6ba6
syhan <syhannnn@gmail.com>
2017-05-17 22:46:49 +0800

    Assignment 1 (but exclude the test data)

--- example/src/test/scala/example/ListsSuite.scala
+++ example/src/test/scala/example/ListsSuite.scala
@@ -47,7 +47,7 @@ import org.scalatest.junit.JUnitRunner
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 != 3) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ import org.scalatest.junit.JUnitRunner
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 !== 3) // Fix me, please!
   }
 
   /**
